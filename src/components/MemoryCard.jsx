38271c8a5e4baa12fac4d9c600c01e67bdffb985
David Carlisle <dave_0021@yahoo.com>
2020-10-27 17:45:17 -0500

    removed regular bootstrap import

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import './MemoryCard.css'
 import logo from './dc-logo.png'
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default class MemoryCard extends Component {
     render() {
